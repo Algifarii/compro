3b00b748a5f1be6fac5c29c7a8e67ed885acfa1d
ALGIFARI <abudzar300@gmail.com>
2024-03-06 09:56:07 +0700

    v 1.0

---
+++ application/views/home/list.php
@@ -0,0 +1,14 @@
+<?php 
+include('slider.php');
+include('about.php');
+include('video.php');
+include('layanan.php');
+include('berita.php');
+?>
+
+
+
+
+
+
+
---
+++ application/views/home/video.php
@@ -0,0 +1,16 @@
+<!-- Start campaian video Section -->
+
+<!-- <section class="bg-compaian-video">
+    <div class="compaian-video-overlay">
+        <div class="container">
+            <div class="row">
+                <div class="compaian-video">
+                    <a href="https://www.youtube.com/embed/3TJAtazhXTs" data-rel="lightcase:myCollection"><img src="<?php echo base_url() ?>assets/tema/assets/images/home02/video-icon.png" alt="video-icon" /></a>
+                    <h3>COMPANY PROFILE GHANIM</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</section> -->
+
+<!-- End campaian video Section -->
